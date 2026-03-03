f5185a9ad08689b478c384b85a3f7442e55328da
Yash Revankar <yashrevankar17@gmail.com>
2026-03-03 22:49:30 +0530

    App deployment

--- src/utils.py
+++ src/utils.py
@@ -22,82 +22,39 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        params = {
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            "Decision Tree": {
-                "max_depth": [5, 10, 20]
-            },
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            "Random Forest": {
-                "n_estimators": [50, 100],
-                "max_depth": [5, 10]
-            },
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            "Linear Regression": {
-                "fit_intercept": [True, False]
-            },
+            y_train_pred = model.predict(X_train)
 
-            "XGBRegressor": {
-                "n_estimators": [50, 100],
-                "learning_rate": [0.01, 0.1]
-            },
+            y_test_pred = model.predict(X_test)
 
-            "CatBoosting Regressor": {
-                "depth": [6, 10],
-                "learning_rate": [0.01, 0.1]
-            },
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            "AdaBoost Regressor": {
-                "n_estimators": [50, 100],
-                "learning_rate": [0.01, 0.1]
-            }
+            test_model_score = r2_score(y_test, y_test_pred)
 
-        }
-
-
-        for model_name, model in models.items():
-
-            print(f"Training {model_name}")
-
-
-            param_grid = params[model_name]
-
-
-            grid = GridSearchCV(
-
-                estimator=model,
-
-                param_grid=param_grid,
-
-                cv=3,
-
-                scoring="r2",
-
-                n_jobs=-1
-
-            )
-
-
-            grid.fit(X_train, y_train)
-
-
-            best_model = grid.best_estimator_
-
-
-            y_pred = best_model.predict(X_test)
-
-
-            score = r2_score(y_test, y_pred)
-
-
-            report[model_name] = score
-
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
